bb75b0bc6065c2d58fea40d8d4cec1aaee22fbd6
burningtree <burningtree@users.noreply.github.com>
2022-05-02 00:37:50 +0000

    deploy: b9101e0328f2b3e10a1f9bf62af6d510d7171ac6

--- _app/chunks/orders-bcddafda.js
+++
@@ -1,1 +0,0 @@
-import{u as o,a as e}from"./stores-9ca2ce08.js";import{a}from"./api-ef402701.js";async function d(t){if(t.orders&&t.orders.length>0||t.tickets&&t.tickets.length>0){const r=await a.apiCall("orders",{method:"POST"},{orders:t.orders,tickets:t.tickets});o.update(s=>(s.orders=r.orders,s.tickets=r.tickets,s))}}async function n(){e.set(await a.apiCall("status"))}export{d as a,n as l};
